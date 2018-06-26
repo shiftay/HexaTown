4e3e2e61999c6bf6475cdaacfc6691df3e17a560
shiftay <shroebuck@gmail.com>
2018-06-26 14:54:08 -0400

    Update to background, basic game manager layout established.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+	public int currentHand, currentTurn, objectiveVal, populationVal;
+	public int cardsPlayed;
+	public bool turnOver = false;
+	public List<int> currentDeck;
+	public List<int> currentDiscard;
+	public List<int> activeSpells; // used to track current player spells for lasting effects.
+	public List<int> currentDEBUFFs; // used to track events.
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	// more complex shuffle @ https://stackoverflow.com/questions/273313/randomize-a-listt
+	void Shuffle() {
+		for (int i = 0; i < currentDeck.Count; i++) {
+        	int temp = currentDeck[i];
+        	int randomIndex = Random.Range(i, currentDeck.Count);
+        	currentDeck[i] = currentDeck[randomIndex];
+        	currentDeck[randomIndex] = temp;
+    	}
+	}
+}
---
+++ Assets/Scripts/BackEndScripts/Background.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+	public Sprite[] dirtTerrain;
+	public SpriteRenderer[] bgSlots;
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+	{
+		for(int i = 0; i < bgSlots.Length; i++) {
+			bgSlots[i].sprite = dirtTerrain[Random.Range(0, dirtTerrain.Length)];
+		}
+	}
+
+}
