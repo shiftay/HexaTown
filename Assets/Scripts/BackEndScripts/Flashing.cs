ff77ba3b7fb8ede9cbffea32777d2daa88dae27a
shiftay <shroebuck@gmail.com>
2018-06-26 18:20:35 -0400

    Update to Scene

---
+++ Assets/Scripts/BackEndScripts/Flashing.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashing : MonoBehaviour {
+
+	SpriteRenderer srFlash;
+
+	bool flash = true;	
+	public float dTime;
+	public float TIME = 0;
+	bool dir = false;
+
+	// Use this for initialization
+	void Start () {
+		srFlash = GetComponent<SpriteRenderer>();
+		TIME = dTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(srFlash.sprite == null && flash) {
+			flash = false;
+		}
+
+		if(dir) {
+			TIME += Time.deltaTime;
+		} else {
+			TIME -= Time.deltaTime;
+		}
+
+
+
+		if(flash) {
+			srFlash.color = Color.Lerp(Color.white, Color.black, TIME);
+		}
+
+		if(TIME >= 1) {
+			dir = false;
+		} else if(TIME <= 0) {
+			dir = true;
+		}
+
+
+
+	}
+}
