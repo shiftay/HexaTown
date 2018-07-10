057ad3c4bbf26adafeb70e451c1e7353ef85e613
shiftay <shroebuck@gmail.com>
2018-07-10 19:30:46 -0400

    Setup the UI for Main Menu, and pregame menu.

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+	public GameObject resumeBtn;
+
+	/// <summary>
+	/// This function is called when the object becomes enabled and active.
+	/// </summary>
+	void OnEnable()
+	{
+		//TODO: Check if there is a saved game ot continue
+	}
+
+
+
+	public void Resume() {
+
+	}
+
+	public void Play() {
+
+	}
+
+	public void Options() {
+		
+	}
+}
---
+++ Assets/TrailFix.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailFix : MonoBehaviour {
+	public TrailRenderer[] tRenderers;
+	public SpriteRenderer sprite;
+	public Sprite[] sprites;
+
+	// Use this for initialization
+	void Start () {
+		sprite = GetComponent<SpriteRenderer>();
+		tRenderers = GetComponentsInChildren<TrailRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeObject() {
+		int r = UnityEngine.Random.Range(0,sprites.Length);
+
+		sprite.sprite = sprites[r];
+
+		foreach(TrailRenderer tr in tRenderers) {
+			float alpha = 1.0f;
+			Gradient gradient = new Gradient();
+
+			switch(r) {
+				case 0:
+							gradient.SetKeys(
+				new GradientColorKey[] { new GradientColorKey(Color.green, 0.0f), new GradientColorKey(Color.white, 1.0f) },
+				new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+				);
+					break;
+				case 1:
+							gradient.SetKeys(
+				new GradientColorKey[] { new GradientColorKey(Color.gray, 0.0f), new GradientColorKey(Color.white, 1.0f) },
+				new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+				);
+					break;
+				case 2:
+							gradient.SetKeys(
+				new GradientColorKey[] { new GradientColorKey(Color.black, 0.0f), new GradientColorKey(Color.white, 1.0f) },
+				new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+				);
+					break;
+			}
+
+
+			tr.colorGradient = gradient;
+
+		}
+	}
+}
